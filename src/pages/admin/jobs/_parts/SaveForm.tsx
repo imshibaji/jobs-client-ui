d1456204235281e6bef3dd29cd114b2e2ea308f8
Shibaji Debnath <imshibaji@gmail.com>
2025-12-04 19:21:13 +0530

    Jobs Sections Updated

---
+++ src/pages/admin/jobs/_parts/SaveForm.tsx
@@ -0,0 +1,215 @@
+import { Company, Currency, EmploymentType, Experience, Job, SalaryRange, SalaryType } from "@/utils/types/Company";
+import { User } from "@/utils/types/User";
+import { useHttpClient } from "@/utils/useHttpClient";
+import { BASE_URL } from "astro:env/client";
+import { useEffect, useState } from "react";
+
+export default function SaveForm({token, jobId}: {token: string, jobId?: string | number}) {
+    const {post, put, get} = useHttpClient(token);
+    const [companies, setCompanies] = useState<Company[]>([]);
+    const [users, setUsers] = useState<User[]>([]);
+    const [form, setForm] = useState<Job>({
+        title: '',
+        description: '',
+        experience: Experience.NO_EXPERIENCE,
+        employmentType: EmploymentType.FULL_TIME,
+        requirements: '',
+        responsibilities: '',
+        benefits: '',
+        skills: '',
+        location: '',
+        salary: '',
+        currency: Currency.INR,
+        salaryType: SalaryType.MONTHLY,
+        salaryRange: SalaryRange.LESS_THAN_10K,
+        isRemote: false,
+        companyId: 1,
+        userId: 1
+    });
+
+    useEffect(() => {
+        onInit();
+    }, [jobId]);
+
+    const onInit = () => {
+        if (jobId) {
+            get(BASE_URL + '/jobs/' + jobId)
+            .then((data: Response) => {
+                return data.json();
+            })
+            .then((data: Job) => {
+                setForm(data);
+            }).catch((err: Error) => {
+                console.log(err);
+            });
+        }
+        get(BASE_URL + '/companies')
+        .then((data: Response) => {
+            return data.json();
+        })
+        .then((data: Company[]) => {
+            // console.log(data);
+            setCompanies(data);
+        }).catch((err: Error) => {
+            console.log(err);
+        });
+        get(BASE_URL + '/users')
+        .then((data: Response) => {
+            return data.json();
+        })
+        .then((data: User[]) => {
+            // console.log(data);
+            setUsers(data);
+        }).catch((err: Error) => {
+            console.log(err);
+        });
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (jobId) {
+            put(BASE_URL + '/jobs/' + jobId, form).then((data) => {
+                console.log(data);
+            });
+        }else{
+            post(BASE_URL + '/jobs', form).then((data) => {
+                console.log(data);
+            });
+        }
+        window.location.href = '/admin/jobs/'+jobId;
+    }
+
+    return (
+    <div className="bg-white rounded-xl shadow p-6 mb-6 mt-4">
+        <div className="flex justify-between items-center mb-4">
+            {
+                jobId ? 
+                    <h2 className="text-3xl font-semibold text-purple-700">Update Job</h2> 
+                : <h2 className="text-3xl font-semibold text-purple-700">Create A Job</h2>
+            }
+            <a href="/admin/jobs" className="text-purple-600 focus:ring-2 focus:outline-none focus:ring-purple-300 font-bold rounded-lg text-sm px-5 py-1.5 text-center border border-purple-600 hover:bg-purple-600 hover:text-white">Back To Jobs</a>
+        </div>
+        <hr className="border-gray-300 my-8" />
+        <div className="space-y-3">
+            <form onSubmit={handleSubmit} className="space-y-3">
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-4 items-end">
+                    <div className="flex flex-col col-span-2 md:col-span-1">
+                        <label htmlFor="manager">Select A Manager</label>
+                        <select value={form.userId} onChange={(e) => setForm({...form, userId: Number(e.target.value)})} id="manager" name="userId" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5">
+                            <option selected>Select A Manager</option>
+                            { users && users.map((user) => (
+                                <option key={user.id} value={user.id}>{user.name}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="flex flex-col">
+                        <label htmlFor="company">Choose A Company</label>
+                        <select value={form.companyId} onChange={(e) => setForm({...form, companyId: Number(e.target.value)})}id="company" name="companyId" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5">
+                            <option selected>Choose A Company</option>
+                            { companies && companies.map((company) => (
+                                <option key={company.id} value={company.id}>{company.name}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="w-full flex justify-end">
+                        <a href="/admin/companies/create" className="w-full bg-purple-600 text-white font-semibold py-3 px-4 rounded-md purple-600 hover:text-gray-100 focus:ring-2 focus:outline-none focus:ring-purple-300 text-sm text-center">Create A Company</a>
+                    </div>
+                </div>
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-4 pt-4">
+                    <div className="flex flex-col w-full col-span-2 md:col-span-1">
+                        <label htmlFor="title">Job Title</label>
+                        <input value={form.title} onChange={(e) => setForm({...form, title: e.target.value})} type="text" placeholder="Job Title" id="title" name="title" className="w-full rounded-md border border-gray-300 px-4 py-2 focus:border-purple-500 focus:ring-purple-500" />
+                    </div>
+                    <div className="flex flex-col w-full">
+                        <label htmlFor="title">Employment Type</label>
+                        <select value={form.employmentType} onChange={(e) => setForm({...form, employmentType: e.target.value})} id="title" name="employmentType" className="w-full bg-gray-50 rounded-md border border-gray-300 px-4 py-2 focus:border-purple-500 focus:ring-purple-500">
+                            <option selected>Employment Type</option>
+                            { Object.values(EmploymentType).map((employmentType) => (
+                                <option key={employmentType} value={employmentType}>{employmentType}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="flex flex-row items-center gap-2 w-full">
+                        <input checked={form.isRemote} onChange={(e) => setForm({...form, isRemote: e.target.checked})} type="checkbox" id="isRemote" name="isRemote" className="w-4 h-4 text-purple-600 bg-gray-100 border-gray-300 rounded focus:ring-purple-500 focus:ring-2 focus:border-purple-500" />
+                        <label htmlFor="isRemote">Remote Work Available</label>
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="description">Job Description</label>
+                    <textarea value={form.description} onChange={(e) => setForm({...form, description: e.target.value})} rows={5} placeholder="Job Description" id="description" name="description" className="rounded-md border border-gray-300 px-4 py-2 focus:border-purple-500 focus:ring-purple-500" />
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="requirements">Requirements</label>
+                    <input value={form.requirements} onChange={(e) => setForm({...form, requirements: e.target.value})} type="text" placeholder="Requirements" id="requirements" name="requirements" className="rounded-md border border-gray-300 px-4 py-2 focus:border-purple-500 focus:ring-purple-500" />
+                </div>
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-4 items-end">
+                    <div className="flex flex-col">
+                        <label htmlFor="skills">Skills</label>
+                        <input value={form.skills} onChange={(e) => setForm({...form, skills: e.target.value})} type="text" placeholder="Skills" id="skills" name="skills" className="rounded-md border border-gray-300 px-4 py-2 focus:border-purple-500 focus:ring-purple-500" />
+                    </div>
+                    <div className="flex flex-col">
+                        <label htmlFor="education">Responsibilities</label>
+                        <input value={form.responsibilities} onChange={(e) => setForm({...form, responsibilities: e.target.value})} type="text" placeholder="Responsibilities" id="responsibilities" name="responsibilities" className="rounded-md border border-gray-300 px-4 py-2 focus:border-purple-500 focus:ring-purple-500" />
+                    </div>
+                    <div className="flex flex-col">
+                        <label htmlFor="experience">Experience</label>
+                        <select value={form.experience} onChange={(e) => setForm({...form, experience: e.target.value})} id="experience" name="experience" className="rounded-md border border-gray-300 px-4 py-2 focus:border-purple-500 focus:ring-purple-500">
+                            <option selected>Select Experience</option>
+                            { Object.values(Experience).map((experience) => (
+                                <option key={experience} value={experience}>{experience}</option>
+                            ))}
+                        </select>
+                    </div>
+                    
+                    <div className="flex flex-col col-span-1 md:col-span-2">
+                        <label htmlFor="benefits">Benefits</label>
+                        <input value={form.benefits} onChange={(e) => setForm({...form, benefits: e.target.value})} type="text" placeholder="Benefits" id="benefits" name="benefits" className="rounded-md border border-gray-300 px-4 py-2 focus:border-purple-500 focus:ring-purple-500" />
+                    </div>
+                    
+                    <div className="flex flex-col">
+                        <label htmlFor="location">Job Location</label>
+                        <input value={form.location} onChange={(e) => setForm({...form, location: e.target.value})} type="text" placeholder="Location" id="location" name="location" className="rounded-md border border-gray-300 px-4 py-2 focus:border-purple-500 focus:ring-purple-500" />
+                    </div>
+                </div>
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-4 items-end">
+                    <div className="flex flex-row items-center gap-2 w-full">
+                        <div className="flex flex-col">
+                            <label htmlFor="currency">Currency</label>
+                            <select value={form.currency} onChange={(e) => setForm({...form, currency: e.target.value})} id="currency" name="currency" className="rounded-md border border-gray-300 px-4 py-2 focus:border-purple-500 focus:ring-purple-500">
+                                <option selected>Select Currency</option>
+                                { Object.values(Currency).map((currency) => (
+                                    <option key={currency} value={currency}>{currency}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="flex flex-col flex-1">
+                            <label htmlFor="salary">Salary</label>
+                            <input value={form.salary} onChange={(e) => setForm({...form, salary: e.target.value})} type="text" placeholder="Salary" id="salary" name="salary" className="rounded-md border border-gray-300 px-4 py-2 focus:border-purple-500 focus:ring-purple-500" />
+                        </div>
+                    </div>
+                    <div className="flex flex-col">
+                        <label htmlFor="type">Salary Type</label>
+                        <select value={form.salaryType} onChange={(e) => setForm({...form, salaryType: e.target.value})} id="type" name="type" className="rounded-md border border-gray-300 px-4 py-2 focus:border-purple-500 focus:ring-purple-500">
+                            <option selected>Select Salary Type</option>
+                            { Object.values(SalaryType).map((type) => (
+                                <option key={type} value={type}>{type}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="flex flex-col">
+                        <label htmlFor="link">Salary Range</label>
+                        <select value={form.salaryRange} onChange={(e) => setForm({...form, salaryRange: e.target.value})} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5">
+                            <option selected>Select Salary Range</option>
+                            { Object.values(SalaryRange).map((range) => (
+                                <option key={range} value={range}>{range}</option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+                
+                <button type="submit" className="bg-purple-600 text-white font-semibold py-2 px-4 rounded-md hover:bg-purple-700">Save Job</button>
+            </form>
+        </div>
+    </div>
+    );
+}
--- src/utils/types/Company.ts
+++ src/utils/types/Company.ts
@@ -1,3 +1,5 @@
+import { User } from "./User";
+
 export interface Company {
     id?: number;
     name: string;
@@ -16,23 +18,132 @@ export interface Company {
     userId?: number;
 }
 
+export enum EmploymentType {
+    FULL_TIME = 'Full-time',
+    PART_TIME = 'Part-time',
+    CONTRACT = 'Contract',
+    INTERNSHIP = 'Internship',
+    TEMPORARY = 'Temporary',
+    VOLUNTEER = 'Volunteer',
+    OTHER = 'Other',
+}
+
+export enum SalaryType {
+    HOURLY = 'Hourly',
+    DAILY = 'Daily',
+    WEEKLY = 'Weekly',
+    MONTHLY = 'Monthly',
+    YEARLY = 'Yearly',
+}
+
+export enum SalaryRange {
+    LESS_THAN_10K = '<10k',
+    BETWEEN_10K_AND_20K = '10k-20k',
+    BETWEEN_20K_AND_30K = '20k-30k',
+    BETWEEN_30K_AND_40K = '30k-40k',
+    BETWEEN_40K_AND_50K = '40k-50k',
+    BETWEEN_50K_AND_60K = '50k-60k',
+    BETWEEN_60K_AND_70K = '60k-70k',
+    BETWEEN_70K_AND_80K = '70k-80k',
+    BETWEEN_80K_AND_90K = '80k-90k',
+    BETWEEN_90K_AND_100K = '90k-100k',
+    MORE_THAN_100K = '>100k',
+}
+
+export enum Currency {
+    INR = 'INR',
+    USD = 'USD',
+    EUR = 'EUR',
+    GBP = 'GBP',
+    AUD = 'AUD',
+    CAD = 'CAD',
+    CNY = 'CNY',
+    JPY = 'JPY',
+    KRW = 'KRW',
+    MXN = 'MXN',
+    RUB = 'RUB',
+    TRY = 'TRY',
+    ZAR = 'ZAR',
+}
+
+export enum IndustryType {
+    FINANCE = 'Finance',
+    TECHNOLOGY = 'Technology',
+    HEALTHCARE = 'Healthcare',
+    EDUCATION = 'Education',
+    MANUFACTURING = 'Manufacturing',
+    RETAIL = 'Retail',
+    SERVICES = 'Services',
+    REAL_ESTATE = 'Real Estate',
+    CONSTRUCTION = 'Construction',
+    MEDIA = 'Media',
+    AGRICULTURE = 'Agriculture',
+    OTHER = 'Other',
+}
+
+export enum Size {
+    employees_1_10 = '1 - 10 Employees',
+    employees_11_50 = '11 - 50 Employees',
+    employees_51_200 = '51 - 200 Employees',
+    employees_201_500 = '201 - 500 Employees',
+    employees_501_1000 = '501 - 1000 Employees',
+    employees_1001_5000 = '1001 - 5000 Employees',
+    employees_5001_10000 = '5001 - 10000 Employees',
+    employees_10001_plus = '10001+ Employees',
+}
+
+export enum Experience {
+    NO_EXPERIENCE = 'No Experience',
+    ONE_YEAR = '1 Year',
+    TWO_YEARS = '2 Years',
+    THREE_YEARS = '3 Years',
+    FOUR_YEARS = '4 Years',
+    FIVE_YEARS = '5 Years',
+    SIX_YEARS = '6 Years',
+    SEVEN_YEARS = '7 Years',
+    EIGHT_YEARS = '8 Years',
+    NINE_YEARS = '9 Years',
+    TEN_YEARS = '10 Years',
+    MORE_THAN_10_YEARS = 'More than 10 Years',
+}
+
+export enum Gender {
+    MALE = 'Male',
+    FEMALE = 'Female',
+    OTHER = 'Other',
+}
+
+
 export interface Job {
     id?: number;
     title: string;
     description: string;
+    experience?: string;
+    employmentType: string;
     requirements: string;
     responsibilities: string;
     benefits: string;
     skills?: string;
     location?: string;
+    currency?: string;
     salary?: string;
     salaryType?: string;
     salaryRange?: string;
-    employmentType: string;
-    experience?: string;
     isRemote?: boolean;
     companyId?: number;
     userId?: number;
+}
+
+export function getCompany( companies: Company[], id: number | string): Company {
+    return companies.filter((company: Company) => company.id === id)[0];
+}
+
+export function getJob( jobs: Job[], id: number | string): Job {
+    return jobs.filter((job: Job) => job.id === id)[0];
+}
+
+export function getUser( users: User[], id: number | string): User {
+    return users.filter((user: User) => user.id === id)[0];
 }
 
 export interface Application {
